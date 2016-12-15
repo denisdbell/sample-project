f10e508215bedec3fd5157711c490f4d08ebface
José Sevila <josevilah@gmail.com>
2016-12-15 15:04:15 -0500

    create test case for create an Activiti

--- src/main/java/com/advantum/activity/service/ActivityService.java
+++ src/main/java/com/advantum/activity/service/ActivityService.java
@@ -1,8 +1,12 @@
 package com.advantum.activity.service;
 
+import com.advantum.activity.model.Activity;
 import com.advantum.activity.repository.ActivityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
 
 /**
  * Created by Sevila <josevilah@gmail.com> on 15/12/2016.
@@ -12,5 +16,12 @@ public class ActivityService {
 
     @Autowired
     private ActivityRepository activityRepository;
-    
+
+    public Activity add(Activity activity) {
+        throw new NotImplementedException();
+    }
+
+    public List<Activity> list() {
+        throw new NotImplementedException();
+    }
 }
--- src/test/java/com/advantum/activity/service/ActivityServiceIntegrationTest.java
+++ src/test/java/com/advantum/activity/service/ActivityServiceIntegrationTest.java
@@ -1,12 +1,21 @@
 package com.advantum.activity.service;
 
 import com.advantum.App;
-import com.advantum.activity.repository.ActivityRepository;
+import com.advantum.activity.model.*;
+import com.advantum.activity.repository.ActivityTypeRepository;
+import com.advantum.activity.repository.UserRepository;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by Sevila <josevilah@gmail.com> on 15/12/2016.
@@ -18,6 +27,27 @@ import org.springframework.transaction.annotation.Transactional;
 public class ActivityServiceIntegrationTest {
 
     @Autowired
-    private ActivityRepository activityRepository;
+    private ActivityService activityService;
 
+    @Autowired
+    private ActivityTypeRepository activityTypeRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    @Sql("../repository/dummy-data.sql")
+    public void testCreateActivity() {
+        ActivityType activityType = activityTypeRepository.getOne(1);
+        User user = userRepository.getOne(1);
+        Activity activity = new Activity(1, "Activity Created from Service", new Date(), new Date(), "test", activityType, new ArrayList<ActivityDelay>(), user);
+
+        activityService.add(activity);
+
+        List<Activity> activityList = activityService.list();
+        Assert.assertTrue("There is one created activity", activityList.size() == 1);
+
+        Activity activityFound = activityList.get(0);
+        Assert.assertEquals("The found activity is the created one", activityFound.getActivityId(), activity.getActivityId());
+    }
 }
