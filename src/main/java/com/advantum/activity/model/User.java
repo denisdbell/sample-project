045c5a2634c6d412398d2eead7417a3286d9237a
José Sevila <josevilah@gmail.com>
2016-12-14 23:21:19 -0500

    create User entity; create creation and initialization scripts for _user table

---
+++ src/main/java/com/advantum/activity/model/User.java
@@ -0,0 +1,44 @@
+package com.advantum.activity.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by Sevila <josevilah@gmail.com> on 14/12/2016.
+ */
+@Entity
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    private Integer userId;
+
+    private String username;
+
+    public User() {
+    }
+
+    public User(Integer userId, String username) {
+        this.userId = userId;
+        this.username = username;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
