fb7604b458a0e63a62636684e761593d00206d6d
José Sevila <josevilah@gmail.com>
2016-12-16 12:10:11 -0500

    refactors methods in ActivityRestControllerWebIntegrationTest creates test for update method

--- src/test/java/com/advantum/activity/controller/rest/ActivityRestControllerWebIntegrationTest.java
+++ src/test/java/com/advantum/activity/controller/rest/ActivityRestControllerWebIntegrationTest.java
@@ -5,7 +5,6 @@ import com.advantum.activity.model.Activity;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
@@ -14,9 +13,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -32,39 +29,47 @@ public class ActivityRestControllerWebIntegrationTest {
 
     @Test
     public void testFindOneActivity() {
-        RestTemplate restTemplate = new TestRestTemplate();
-        ResponseEntity<Activity> response = restTemplate.getForEntity("http://localhost:8282/api/activities/1", Activity.class);
-        Activity foundActivity = response.getBody();
+        Activity foundActivity = getActivityRest(1);
         Assert.assertNotNull("An Activity was found", foundActivity);
         Assert.assertEquals("The \"Activity description 01\" should be found", "Activity description 01", foundActivity.getDescription());
     }
 
+    private Activity getActivityRest(Integer activityId) {
+        RestTemplate restTemplate = new TestRestTemplate();
+        ResponseEntity<Activity> response = restTemplate.getForEntity("http://localhost:8282/api/activities/" + activityId, Activity.class);
+        return response.getBody();
+    }
+
     @Test
     public void testExistentActivityDelaysInFoundActivity() {
-        RestTemplate restTemplate = new TestRestTemplate();
-        ResponseEntity<Activity> response = restTemplate.getForEntity("http://localhost:8282/api/activities/1", Activity.class);
-        Activity foundActivity = response.getBody();
+        Activity foundActivity = getActivityRest(1);
         Assert.assertNotNull("An Activity was found", foundActivity);
         Assert.assertEquals("The \"The activity was delayed\" ActivityDelay should be found", "The activity was delayed", foundActivity.getActivityDelays().get(0).getReason());
     }
 
     @Test
     public void testUpdateActivity() {
-        RestTemplate restTemplate = new TestRestTemplate();
-        ResponseEntity<Activity> response = restTemplate.getForEntity("http://localhost:8282/api/activities/1", Activity.class);
-        Activity activity = response.getBody();
+        Activity activity = getActivityRest(1);
         activity.setDescription("Modified activity");
-        HttpEntity<Activity> updateRequestEntity = new HttpEntity<Activity>(activity);
-        restTemplate.exchange("http://localhost:8282/api/activities/1", HttpMethod.PUT, updateRequestEntity, Activity.class);
-        response = restTemplate.getForEntity("http://localhost:8282/api/activities/1", Activity.class);
-        activity = response.getBody();
+
+        updateActivityRest(activity);
+
+        activity = getActivityRest(1);
+
         Assert.assertNotNull(activity);
         Assert.assertEquals("The activity was modified", "Modified activity", activity.getDescription());
+
         activity.setDescription("Activity description 01");
 
-        updateRequestEntity = new HttpEntity<Activity>(activity);
-        response = restTemplate.exchange("http://localhost:8282/api/activities/1", HttpMethod.PUT, updateRequestEntity, Activity.class);
-        Assert.assertEquals("The activity was modified back", "Activity description 01", response.getBody().getDescription());
+        activity = updateActivityRest(activity);
+        Assert.assertEquals("The activity was modified back", "Activity description 01", activity.getDescription());
+    }
+
+    private Activity updateActivityRest(Activity activity) {
+        HttpEntity<Activity> updateRequestEntity = new HttpEntity<Activity>(activity);
+        RestTemplate restTemplate = new TestRestTemplate();
+        ResponseEntity<Activity> responseEntity = restTemplate.exchange("http://localhost:8282/api/activities/1", HttpMethod.PUT, updateRequestEntity, Activity.class);
+        return responseEntity.getBody();
     }
 
     @Test
